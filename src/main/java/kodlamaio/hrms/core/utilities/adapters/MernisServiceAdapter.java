204a3cdd022ca1e5c8ac2de6daef3e7ef5d9b407
Zzeynepyilmaz <77531493+Zzeynepyilmaz@users.noreply.github.com>
2021-06-06 22:51:30 +0300

    Day 8

--- src/main/java/kodlamaio/hrms/core/utilities/adapters/MernisServiceAdapter.java
+++ src/main/java/kodlamaio/hrms/core/utilities/adapters/MernisServiceAdapter.java
@@ -1,7 +1,5 @@
 package kodlamaio.hrms.core.utilities.adapters;
 
-import java.util.regex.Pattern;
-
 public class MernisServiceAdapter {
 
 	public boolean validatePerson(String identity, String firstName, String lastName, int birtDate) {
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="cities")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","JobAdvertisement"})
+public class City {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="city_name")
+	private String cityName;
+}
